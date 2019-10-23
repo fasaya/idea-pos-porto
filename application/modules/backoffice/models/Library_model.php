e00d3206bf7810d632052e9845810acea8e6dd9f
fasaya <aya_maruf@yahoo.com>
2019-10-23 16:42:26 +0800

    modifier belum selesai

--- application/modules/backoffice/models/Library_model.php
+++ application/modules/backoffice/models/Library_model.php
@@ -94,6 +94,17 @@ class Library_model extends CI_Model
 
     function addModifier($tipe, $data)
     {
+        // CONTOH
+        // $array_values = "";
+        // if (isset($_POST["input_array_name"]) && is_array($_POST["input_array_name"])) {
+        //     $input_array_name = array_filter($_POST["input_array_name"]);
+        //     foreach ($input_array_name as $field_value) {
+        //         $array_values .= $field_value . "<br />";
+        //     }
+        // }
+        // END CONTOH
+
+
         if ($tipe == "saveall") {
             $outlet = $this->get_outlet()->result();
             foreach ($outlet as $o) {
